97b2279b2cb9150bc1cf76cd4a0dd2f73c3f42b7
Mikael Voss <mvs@nyantec.com>
2015-03-23 18:29:52 +0100

    Initial commit

---
+++ narwhal.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+# Docker container ID
+id="$(docker inspect --format '{{ .Id }}' "$1" | cut -c 1-12)"
+
+# Interface names
+ext="nw-$id"
+int="nwt-$$"
+
+# IPv4 addresses
+ipv4="$2"
+gwv4="$3"
+
+# IPv6 addresses
+ipv6="$4"
+gwv6="$5"
+
+# Temporary network namespace name
+ns="$int"
+
+# Determine Docker container PID
+pid="$(docker inspect --format='{{ .State.Pid }}' "$id")"
+
+# Create temporary name for network namespace
+ln -f -s "/proc/$pid/ns/net" "/var/run/netns/$ns"
+
+# Create veth pair
+ip link add "$ext" type veth peer name "$int"
+
+# Move container device to container namespace
+ip link set "$int" netns "$ns"
+
+# Rename device inside container namespace
+ip netns exec "$ns" ip link set "$int" name eth0
+
+# Set interfaces up
+ip link set "$ext" up
+ip netns exec "$ns" ip link set eth0 up
+
+# Assign container IPv4 address
+ip netns exec "$ns" ip -4 address add "$ipv4" peer "$gwv4/32" dev eth0
+ip netns exec "$ns" ip -4 route add default via "$gwv4"
+
+# Assign host IPv4 address
+ip -4 address add "$gwv4" peer "$ipv4/32" dev "$ext"
+
+# Assign container IPv6 addresses
+ip netns exec "$ns" ip -6 address add "$ipv6" peer "$gwv6/128" dev eth0
+ip netns exec "$ns" ip -6 route add default via "$gwv6"
+
+# Assign host IPv6 address
+ip -6 addr add "$gwv6" peer "$ipv6/128" dev "$ext"
+
+# Remove temporary network namespace
+rm -f "/var/run/netns/$ns"
